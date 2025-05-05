6f0117e463e65ef2274b5607277ca0fd1842505d
Pat <harkle@gmx.net>
2025-05-05 19:56:37 +0200

    fixing

--- devices-display/setup.sh
+++ devices-display/setup.sh
@@ -18,7 +18,7 @@ echo "📦 Installing required system packages..."
 sudo apt update
 					
 sudo apt install -y python3 python3-pip libjpeg-dev libopenjp2-7 libopenblas0 \
-  python3-flask python3-requests python3-numpy python3-pillow python3-dotenv python3-spidev python3-spotipy \
+  python3-flask python3-requests python3-numpy python3-pillow python3-dotenv python3-spidev \
  
 # Spotipy (nur über pip verfügbar)
 pip3 install --no-cache-dir spotipy python-dotenv
